88d035e276abde555941af091ffef70ca72fb407
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-05-05 15:50:36 -0700

    Initial commit with all my work so far

---
+++ arrays.js
@@ -0,0 +1,9 @@
+const pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
+
+// for (toppings of pizzaToppings)
+//   console.log(toppings);
+
+// console.log("[ '" + pizzaToppings[0] + "', '" + pizzaToppings[1] + "', '" +pizzaToppings[2] + "' ]");
+
+console.log(pizzaToppings);
+
